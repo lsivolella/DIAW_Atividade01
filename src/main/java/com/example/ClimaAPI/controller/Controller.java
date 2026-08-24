64dd66c81c2d851ce4894fad6f7ff0106fd842a9
Lucas <lucassivolella@gmail.com>
2026-08-24 19:56:36 -0300

    Adiciona formtação para as saídas da API

--- src/main/java/com/example/ClimaAPI/controller/Controller.java
+++ src/main/java/com/example/ClimaAPI/controller/Controller.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ClimaAPI.service.Service;
 
+import tools.jackson.databind.ObjectMapper;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
@@ -11,9 +13,22 @@ public class Controller {
 
     Service service = new Service();
 
-    @GetMapping("/temperatura")
-    public String getTemepratura() {
-        return service.getTemperatura();
+    private String formatarJson(String json) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Object jsonObject = mapper.readValue(json, Object.class);
+            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Erro ao formatar JSON: " + e.getMessage();
+        }
+    }
+
+    @GetMapping(value = "/temperatura", produces = "application/json")
+    public String getTemperatura() {
+        String temperaturaRequest = service.getTemperatura();
+
+        return formatarJson(temperaturaRequest);
     }
 
 }

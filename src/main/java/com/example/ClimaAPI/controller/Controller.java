28d1da0cae7faab195991439823afa396afc8050
Lucas <lucassivolella@gmail.com>
2026-08-24 19:32:53 -0300

    Adiciona suporte para o get temperatura

--- src/main/java/com/example/ClimaAPI/controller/Controller.java
+++ src/main/java/com/example/ClimaAPI/controller/Controller.java
@@ -1,5 +1,19 @@
 package com.example.ClimaAPI.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ClimaAPI.service.Service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
 public class Controller {
+
+    Service service = new Service();
+
+    @GetMapping("/temperatura")
+    public String getTemepratura() {
+        return service.getTemperatura();
+    }
 
 }
--- src/main/java/com/example/ClimaAPI/service/Service.java
+++ src/main/java/com/example/ClimaAPI/service/Service.java
@@ -1,5 +1,31 @@
 package com.example.ClimaAPI.service;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 public class Service {
 
+    private final String LATITUDE_BH = "-19.9297";
+    private final String LONGITUDE_BH = "-43.966034";
+
+    private String consultarURL(String apiURL) {
+        String dados = "";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(apiURL, String.class);
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            dados = responseEntity.getBody();
+        } else {
+            dados = "Falha ao obter dados. Código de status: " + responseEntity.getStatusCode();
+        }
+        return dados;
+    }
+
+    public String getTemperatura() {
+        String apiURL = String.format(
+                "https://api.open-meteo.com/v1/forecast?latitude=%s&longitude=%s&current_weather=true",
+                LATITUDE_BH,
+                LONGITUDE_BH);
+        String dados = consultarURL(apiURL);
+        return dados;
+    }
 }

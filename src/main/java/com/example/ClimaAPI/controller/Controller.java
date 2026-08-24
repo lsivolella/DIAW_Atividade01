7996f86fbcb3cb6d4ca0d13f753a10231fa4b2db
Lucas <lucassivolella@gmail.com>
2026-08-24 20:30:11 -0300

    Adiciona suporte para buscas com o nome de uma cidade

--- src/main/java/com/example/ClimaAPI/controller/Controller.java
+++ src/main/java/com/example/ClimaAPI/controller/Controller.java
@@ -4,9 +4,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ClimaAPI.service.Service;
 
+import tools.jackson.databind.JsonNode;
 import tools.jackson.databind.ObjectMapper;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class Controller {
@@ -22,6 +24,26 @@ public class Controller {
             e.printStackTrace();
             return "Erro ao formatar JSON: " + e.getMessage();
         }
+    }
+
+    @GetMapping(value = "/temperatura/{cidade}", produces = "application/json")
+    public String getTemperatura(@PathVariable String cidade) {
+        JsonNode coordenadasRequest = service.getCoordenadas(cidade);
+
+        String latitude = coordenadasRequest
+                .get("results")
+                .get(0)
+                .get("latitude")
+                .asString();
+
+        String longitude = coordenadasRequest
+                .get("results")
+                .get(0)
+                .get("longitude")
+                .asString();
+
+        String temperaturaRequest = service.getTemperatura(latitude, longitude);
+        return formatarJson(temperaturaRequest);
     }
 
     @GetMapping(value = "/temperatura", produces = "application/json")
--- src/main/java/com/example/ClimaAPI/service/Service.java
+++ src/main/java/com/example/ClimaAPI/service/Service.java
@@ -3,6 +3,9 @@ package com.example.ClimaAPI.service;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
+
 public class Service {
 
     private final String LATITUDE_BH = "-19.9297";
@@ -27,5 +30,25 @@ public class Service {
                 LONGITUDE_BH);
         String dados = consultarURL(apiURL);
         return dados;
+    }
+
+    public String getTemperatura(String latitude, String longitude) {
+        String apiURL = String.format(
+                "https://api.open-meteo.com/v1/forecast?latitude=%s&longitude=%s&current_weather=true",
+                latitude,
+                longitude);
+        String dados = consultarURL(apiURL);
+        return dados;
+    }
+
+    public JsonNode getCoordenadas(String cidade) {
+        String apiURL = String.format(
+                "https://geocoding-api.open-meteo.com/v1/search?name=%s&count=1&language=pt&format=json",
+                cidade.replace(" ", "+"));
+        String dados = consultarURL(apiURL);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        return mapper.readTree(dados);
     }
 }
